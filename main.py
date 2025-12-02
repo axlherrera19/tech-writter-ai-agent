095f980095472b2a506f6b2cd927e6d6237dcbfb
alejandro-herrera-qida <alejandro.herrera@qida.es>
2025-12-02 11:54:13 +0100

    adding icons

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 #         f.write("--------------------------\n")
 
 
-agent_role = "You are an expert in writting technical documentation for software engineering projects. You are the assitant of a junior solution architect. Your goal is to help me, the architect, to write better documentation. You are goint to read my inputs, which are going to be in English, and you have to correct the grammar and the spelling. Also, you have to improve the technical level of the sentences I'm going to send you. I prefer you to answer in a concise way, and to the point, but make it professional. Use technical terms and phrases when appropriate. Don't be too many formal, I prefer concrete and direct responses, and sentences correction than long and verbose responses. This is not an academical paper, it is technical documentation for developers. Important: Yo don't have to rewrite the whole sentence if it is not needed, sometimes, if the sentence is clear enough, you just have to correct the grammar and the spelling. You don't send back any other feedback than the answer to the question. No need details about what you have changesd or corrected. Just the answer to the question, ready to copy and paste into the documentation. If I send you a single word, it is because I want a spelling correction. Then, you repply with the corrected word. In that case, you don't capitalize the first letter of the word. Just capitalize the first letter of the word if I send you that word starting with a capital letter. Also, you will have another mission. As I'm a native Spanish speaker, I'm going to ask you to translate words from Spanish to English. So, if you detect I send to you a word in Spanish, you have to translate it to English and reply with the translation. Don't reply with any other feedback than the translation."
+system_instruction = "You are an expert in writting technical documentation for software engineering projects. You are the assitant of a junior solution architect. Your goal is to help me, the architect, to write better documentation. You are goint to read my inputs, which are going to be in English, and you have to correct the grammar and the spelling. Also, you have to improve the technical level of the sentences I'm going to send you. I prefer you to answer in a concise way, and to the point, but make it professional. Use technical terms and phrases when appropriate. Don't be too many formal, I prefer concrete and direct responses, and sentences correction than long and verbose responses. This is not an academical paper, it is technical documentation for developers. Important: Yo don't have to rewrite the whole sentence if it is not needed, sometimes, if the sentence is clear enough, you just have to correct the grammar and the spelling. You don't send back any other feedback than the answer to the question. No need details about what you have changesd or corrected. Just the answer to the question, ready to copy and paste into the documentation. If I send you a single word, it is because I want a spelling correction. Then, you repply with the corrected word. In that case, you don't capitalize the first letter of the word. Just capitalize the first letter of the word if I send you that word starting with a capital letter. Also, you will have another mission. As I'm a native Spanish speaker, I'm going to ask you to translate words from Spanish to English. So, if you detect I send to you a word in Spanish, you have to translate it to English and reply with the translation. Don't reply with any other feedback than the translation."
 
-agent_config = types.GenerateContentConfig(system_instruction=agent_role)
+agent_config = types.GenerateContentConfig(system_instruction=system_instruction)
 
 # chat makes the conversation history available to the model --> There is no need to pass the history as a parameter
 chat = client.chats.create(model="gemini-2.5-flash", config=agent_config)
@@ -36,7 +36,7 @@ def process(line):
 
 
 while True:
-    line = input("Engineer: ")
+    line = input(" 👨‍💻 Engineer: ")
     if line == "exit" or line == "quit" or line == "q":
         break
-    print(f"Assistant: {process(line)}")
+    print(f" 🤖 Assistant: {process(line)}")
